b7bef227686d6f9e720e4fce9bdac152325ca2ca
Pritham Ganesh <prithamganesh@gmail.com>
2025-12-13 18:56:13 +0530

    Update app.py

--- src/api/app.py
+++ src/api/app.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return jsonify(message="Hello DevOps World!!")
+
+@app.route("/health")
+def health():
+  return jsonify(status="Healthy")
+
+if __name__ == "main":
+  app.run(host="0.0.0.0", port=5000)
